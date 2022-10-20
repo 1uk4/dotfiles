79cf5f769b84bd8f87ec24072e7d5311f62e2237
LukaFlores <luka.flores@mail.com>
2022-10-20 15:17:51 -0700

    lapx command and snippet tab-jump

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -73,13 +73,53 @@ g.vsnip_filetypes = {
   ["typescript.tsx"] = {"typescript"}
 }
 
+-- Standard Snippets
+ Plug "rafamadriz/friendly-snippets"
+
+
 -- Snippets Jump Shortcut
-vim.keymap.set(
-  {"i", "s"},
-  "<C-l>",
-  [[vsnip#jumpable(1) ? "\<Plug>(vsnip-jump-next)" : "\<C-l>"]],
-  { buffer = bufnr, expr = true, replace_keycodes = true, remap = true }
-)
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+
+local check_back_space = function()
+    local col = vim.fn.col('.') - 1
+    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+        return true
+    else
+        return false
+    end
+end
+
+-- Use (s-)tab to:
+--- move to prev/next item in completion menuone
+--- jump to prev/next snippet's placeholder
+--
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif vim.fn.call("vsnip#available", {1}) == 1 then
+    return t "<Plug>(vsnip-expand-or-jump)"
+  elseif check_back_space() then
+    return t "<Tab>"
+  end
+end
+
+_G.s_tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-p>"
+  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+    return t "<Plug>(vsnip-jump-prev)"
+  else
+    return t "<S-Tab>"
+  end
+end
+
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
 -- add color highlighting to hex values
 Plug "norcalli/nvim-colorizer.lua"
@@ -100,7 +140,7 @@ Plug "neovim/nvim-lspconfig"
 Plug "williamboman/nvim-lsp-installer"
 
 --Auto Save
-Plug "Pocco81/AutoSave.nvim"
+Plug "Pocco81/auto-save.nvim"
 
 -- neovim completion
 Plug "hrsh7th/cmp-nvim-lsp"
@@ -174,6 +214,7 @@ plugEnd()
 -- Once the plugins have been loaded, Lua-based plugins need to be required and started up
 -- For plugins with their own configuration file, that file is loaded and is responsible for
 -- starting them. Otherwise, the plugin itself is required and its `setup` method is called.
+require("plugins.auto-save")
 require("plugins.todo-comments")
 require("plugins.gruvbox")
 require("nvim-autopairs").setup()
