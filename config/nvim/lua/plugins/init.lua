f161693932cca83aa1b0577122b6141bb61ab2aa
LukaFlores <luka.flores@mail.com>
2022-10-31 16:13:05 -0700

    fix: remove lspsaga

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -35,8 +35,6 @@ Plug "tpope/vim-rhubarb"
 nmap("<leader>gr", ":Gread<cr>")
 nmap("<leader>gb", ":G blame<cr>")
 
--- Lspsaga
-Plug 'glepnir/lspsaga.nvim'
 
 -- general plugins
 -- Plug 'pangloss/vim-javascript', { 'for': ['javascript', 'javascript.jsx', 'html'] }
