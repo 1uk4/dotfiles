c3370fed73e74c548950b903cbb2126abf76175b
LukaFlores <luka.flores@mail.com>
2023-08-22 10:22:44 -0700

    compatiable with macos

--- config/nvim/after/plugin/vimtex.lua
+++ config/nvim/after/plugin/vimtex.lua
@@ -1,4 +1,18 @@
 vim.g.tex_flavor='latex'
-vim.g.vimtex_view_method="zathura"
 vim.g.vimtex_quickfix_mode= 0 
 vim.g.vimtex_view_automatic = 0
+ vim.g.vimtex_view_method="skim"
+
+--local handle = io.popen("echo $OSTYPE")
+--local result = handle:read("*a")
+--handle:close()
+
+
+--if (string.match(result, "linux-gnu^[*]")) then 
+--    vim.g.vimtex_view_method="zathura"
+--elseif (string.match(result, "darwin^[*]")) then 
+--    vim.g.vimtex_view_method="skim"
+--else
+--    print("Cannot find OS Type")
+--end
+
