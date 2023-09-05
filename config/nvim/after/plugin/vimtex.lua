30fe09bb51d0514e6a112f757f286381df0eebf6
LukaFlores <luka.flores@mail.com>
2023-09-05 13:22:23 -0700

    bin: add clhis for chromium

--- config/nvim/after/plugin/vimtex.lua
+++ config/nvim/after/plugin/vimtex.lua
@@ -6,8 +6,8 @@ vim.g.vimtex_view_automatic = 0
 --local handle = io.popen("echo $OSTYPE")
 --local result = handle:read("*a")
 --handle:close()
-
-
+--
+--
 --if (string.match(result, "linux-gnu^[*]")) then 
 --    vim.g.vimtex_view_method="zathura"
 --elseif (string.match(result, "darwin^[*]")) then 
@@ -15,4 +15,4 @@ vim.g.vimtex_view_automatic = 0
 --else
 --    print("Cannot find OS Type")
 --end
-
+--
