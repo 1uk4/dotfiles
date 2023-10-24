dc23c67b2ab7e5493123e4af03d46b1b7cf34b95
LukaFlores <luka.flores@mail.com>
2023-10-24 10:50:15 +0800

    adir feature

--- config/nvim/after/plugin/vimtex.lua
+++ config/nvim/after/plugin/vimtex.lua
@@ -1,7 +1,7 @@
 vim.g.tex_flavor='latex'
 vim.g.vimtex_quickfix_mode= 0 
 vim.g.vimtex_view_automatic = 0
- vim.g.vimtex_view_method="skim"
+vim.g.vimtex_view_method="zathura"
 
 --local handle = io.popen("echo $OSTYPE")
 --local result = handle:read("*a")
