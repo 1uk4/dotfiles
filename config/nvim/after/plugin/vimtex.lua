ecbb7fb20511474ebab87bf29d8048b1da6f3097
LukaFlores <luka.flores@mail.com>
2023-11-06 00:46:19 -0800

    nvim: latex folding

--- config/nvim/after/plugin/vimtex.lua
+++ config/nvim/after/plugin/vimtex.lua
@@ -2,6 +2,10 @@ vim.g.tex_flavor='latex'
 vim.g.vimtex_quickfix_mode= 0 
 vim.g.vimtex_view_automatic = 0
 vim.g.vimtex_view_method="skim"
+vim.g.vimtex_view_general_viewer = '/Applications/Skim.app/Contents/SharedSupport/displayline'
+vim.g.vimtext_view_genral_options = '-r @line @pdf @tex'
+vim.g.vimtex_fold_enabled = 1
+vim.g.vimtex_fold_manual = 1
 
 --local handle = io.popen("echo $OSTYPE")
 --local result = handle:read("*a")
